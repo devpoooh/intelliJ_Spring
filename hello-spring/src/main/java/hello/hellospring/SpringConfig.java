3819cdf189166a53eae3a0737ebdabefc6e07182
devpoooh <99088509+devpoooh@users.noreply.github.com>
2022-03-21 03:15:27 +0900

    jdbc fail

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,36 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+//직접 스프링 빈 등록하기
+@Configuration //스프링이 올라올 때 빈이 있으면 올려줘라.
+public class SpringConfig {
+
+    //디비와 연결할 수 있는 데이터 소스를 스프링자체에서 만들어 준다.
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean   //스프링 빈을 등록하겠다.
+    public MemberService memberService() {
+        //서비스와 리포지토리 연결
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository(); 저장소
+        return new JdbcMemberRepository(dataSource);
+
+    }
+}
