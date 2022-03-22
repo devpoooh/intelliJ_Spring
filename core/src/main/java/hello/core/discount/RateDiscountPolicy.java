39871fae45d26ece858bbcc5f558f1184875324d
devpoooh <99088509+devpoooh@users.noreply.github.com>
2022-03-22 15:13:11 +0900

    정률 할인 개발

---
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    //정률로 할인 금액
+    public int discount(Member member, int price) {
+        //VIP면
+        if(member.getGrade() == Grade.VIP){
+            return price*discountPercent/100; //가격에 10% 할인
+        }else{ //BASIC이면
+            return 0; //할인금액 없음
+        }
+    }
+}
